058df1b61025d3cb9874949b35945fa3d7e4dd10
david alvarez <david@southfloridaathleticclub.com>
2024-06-06 12:54:45 -0400

    MODELS

--- models/models.go
+++ models/models.go
@@ -2,65 +2,60 @@ package models
 
 import "time"
 
-// Vending machine, ice machine, coffee machine, snack machine, healthy snack machine, etc...
 type VendingType struct {
-	VendingTypeID int    `gorm:"primaryKey;column:vending_type_id" json:"vending_type_id"`
-	MachineType   string `gorm:"unique;not null;column:machine_type" json:"machine_type"`
+	VendingTypeID int    `json:"vending_type_id"`
+	MachineType   string `json:"machine_type"`
 }
 
-// Barbershop, office building, gym, etc...
 type VendingLocation struct {
-	VendingLocationID int    `gorm:"primaryKey;column:vending_location_id" json:"vending_location_id"`
-	LocationType      string `gorm:"unique;not null;column:location_type" json:"location_type"`
+	VendingLocationID int    `json:"vending_location_id"`
+	LocationType      string `json:"location_type"`
 }
 
 type City struct {
-	CityID int    `gorm:"primaryKey;column:city_id" json:"city_id"`
-	Name   string `gorm:"unique;not null;column:name" json:"name"`
+	CityID int    `json:"city_id"`
+	Name   string `json:"name"`
 }
 
 type Lead struct {
-	LeadID            int              `gorm:"primaryKey;column:lead_id" json:"lead_id"`
-	FirstName         string           `gorm:"not null;column:first_name" json:"first_name"`
-	LastName          string           `gorm:"not null;column:last_name" json:"last_name"`
-	PhoneNumber       string           `gorm:"not null;column:phone_number" json:"phone_number"`
-	CreatedAt         int64            `gorm:"not null;column:created_at" json:"created_at"`
-	Rent              string           `gorm:"column:rent" json:"rent"`
-	FootTraffic       int              `gorm:"column:foot_traffic" json:"foot_traffic"`
-	FootTrafficType   string           `gorm:"column:foot_traffic_type" json:"foot_traffic_type"`
-	VendingTypeID     int              `gorm:"column:vending_type_id" json:"vending_type_id"`
-	VendingType       *VendingType     `gorm:"not null;column:vending_type_id;foreignKey:VendingTypeID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"vending_type"`
-	VendingLocationID int              `gorm:"column:vending_location_id" json:"vending_location_id"`
-	VendingLocation   *VendingLocation `gorm:"not null;column:vending_location_id;foreignKey:VendingLocationID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"vending_location"`
-	CityID            int              `gorm:"column:city_id" json:"city_id"`
-	City              *City            `gorm:"not null;column:city_id;foreignKey:CityID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"city"`
-	LeadMarketing     *LeadMarketing   `json:"lead_marketing"`
+	LeadID            int    `json:"lead_id"`
+	FirstName         string `json:"first_name"`
+	LastName          string `json:"last_name"`
+	PhoneNumber       string `json:"phone_number"`
+	CreatedAt         int64  `json:"created_at"`
+	Rent              string `json:"rent"`
+	FootTraffic       int    `json:"foot_traffic"`
+	FootTrafficType   string `json:"foot_traffic_type"`
+	VendingTypeID     int    `json:"vending_type_id"`
+	VendingLocationID int    `json:"vending_location_id"`
+	CityID            int    `json:"city_id"`
+	UserKey           string `json:"user_key"`
 }
 
 type LeadMarketing struct {
-	LeadMarketingID int    `gorm:"primaryKey;column:lead_marketing_id" json:"lead_marketing_id"`
-	LeadID          int    `gorm:"not null;column:lead_id;foreignKey:LeadID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE" json:"lead_id"`
-	Source          string `gorm:"column:source" json:"source"`
-	Medium          string `gorm:"column:medium" json:"medium"`
-	Channel         string `gorm:"column:channel" json:"channel"`
-	LandingPage     string `gorm:"column:landing_page" json:"landing_page"`
-	Keyword         string `gorm:"column:keyword" json:"keyword"`
-	Referrer        string `gorm:"column:referrer" json:"referrer"`
-	Gclid           string `gorm:"column:gclid" json:"gclid"`
-	CampaignID      int    `gorm:"column:campaign_id" json:"campaign_id"`
-	AdCampaign      string `gorm:"column:ad_campaign" json:"ad_campaign"`
-	AdGroupID       int    `gorm:"column:ad_group_id" json:"ad_group_id"`
-	AdGroupName     string `gorm:"column:ad_group_name" json:"ad_group_name"`
-	AdSetID         int    `gorm:"column:ad_set_id" json:"ad_set_id"`
-	AdSetName       string `gorm:"column:ad_set_name" json:"ad_set_name"`
-	AdID            int    `gorm:"column:ad_id" json:"ad_id"`
-	AdHeadline      int    `gorm:"column:ad_headline" json:"ad_headline"`
-	Language        string `gorm:"column:language" json:"language"`
-	OS              string `gorm:"column:os" json:"os"`
-	UserAgent       string `gorm:"column:user_agent" json:"user_agent"`
-	ButtonClicked   string `gorm:"column:button_clicked" json:"button_clicked"`
-	DeviceType      string `gorm:"column:device_type" json:"device_type"`
-	IP              string `gorm:"column:ip" json:"ip"`
+	LeadMarketingID int    `json:"lead_marketing_id"`
+	LeadID          int    `json:"lead_id"`
+	Source          string `json:"source"`
+	Medium          string `json:"medium"`
+	Channel         string `json:"channel"`
+	LandingPage     string `json:"landing_page"`
+	Keyword         string `json:"keyword"`
+	Referrer        string `json:"referrer"`
+	Gclid           string `json:"gclid"`
+	CampaignID      int    `json:"campaign_id"`
+	AdCampaign      string `json:"ad_campaign"`
+	AdGroupID       int    `json:"ad_group_id"`
+	AdGroupName     string `json:"ad_group_name"`
+	AdSetID         int    `json:"ad_set_id"`
+	AdSetName       string `json:"ad_set_name"`
+	AdID            int    `json:"ad_id"`
+	AdHeadline      int    `json:"ad_headline"`
+	Language        string `json:"language"`
+	OS              string `json:"os"`
+	UserAgent       string `json:"user_agent"`
+	ButtonClicked   string `json:"button_clicked"`
+	DeviceType      string `json:"device_type"`
+	IP              string `json:"ip"`
 }
 
 type CSRFToken struct {
