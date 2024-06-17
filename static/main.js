fc085bc7681c3109b20befb670b5812cf73c3744
david alvarez <david@southfloridaathleticclub.com>
2024-06-17 18:08:29 -0400

    main.js

--- static/main.js
+++ static/main.js
@@ -1,6 +1,18 @@
 const telephoneButton = document.getElementById("telephone-button");
+const phoneNumber = telephoneButton.textContent.trim();
+
+telephoneButton.textContent = formatPhoneNumber(phoneNumber);
 
 telephoneButton.addEventListener("click", () => {
-  const phoneNumber = telephoneButton.textContent.trim();
   window.location.href = "tel:" + phoneNumber;
 });
+
+function formatPhoneNumber(phoneNumber) {
+    const cleaned = ('' + phoneNumber).replace(/\D/g, '');
+    const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+    if (match) {
+        return '(' + match[1] + ') ' + match[2] + ' - ' + match[3];
+    }
+
+    return null;
+}
