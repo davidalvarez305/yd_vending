a8e6a8e9c193f1786fd8ae2672f546b92668ed7d
david alvarez <david@southfloridaathleticclub.com>
2024-08-08 18:15:58 -0400

    logs

--- sessions/session.go
+++ sessions/session.go
@@ -18,11 +18,13 @@ func getSessionFromRequest(r *http.Request) (string, error) {
 		return "", err
 	}
 
+	fmt.Printf("cookie: %+v\n", cookie)
 	return cookie.Value, err
 }
 
 func IsNew(r *http.Request) (bool, error) {
 	_, err := r.Cookie(constants.CookieName)
+	fmt.Printf("err: %+v\n", err)
 	if err == http.ErrNoCookie {
 		return true, nil
 	}
@@ -44,6 +46,7 @@ func Get(r *http.Request) (models.Session, error) {
 	if err != nil {
 		return sessions, err
 	}
+	fmt.Printf("sessions: %+v\n", sessions)
 
 	return sessions, nil
 }
