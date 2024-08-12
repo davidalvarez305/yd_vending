32854c14fbc4194cd73f91d759460cd30d274ec7
David Alvarez <87050356+davidalvarez305@users.noreply.github.com>
2024-08-12 13:43:34 -0400

    more user agents

--- helpers/user.go
+++ helpers/user.go
@@ -41,16 +41,47 @@ func GetTokenFromSession(r *http.Request) ([]byte, error) {
 
 func UserAgentIsBot(userAgent string) bool {
 	var botUserAgents = []string{
-		"Googlebot", // Google Bot
-		"Bingbot",   // Bing Bot
-		"Slurp",     // Yahoo Bot
-		"DuckDuckBot", // DuckDuckGo Bot
-		"Bot",       // Generic Bot
-		"crawler",   // Generic Crawler
-		"spider",    // Generic Spider
+		"Googlebot",          // Google Bot
+		"Bingbot",            // Bing Bot
+		"Slurp",              // Yahoo Bot
+		"DuckDuckBot",        // DuckDuckGo Bot
+		"Bot",                // Generic Bot
+		"crawler",            // Generic Crawler
+		"spider",             // Generic Spider
 		"facebookexternalhit", // Facebook Bot
-		"twitterbot", // Twitter Bot
-		"linkedinbot", // LinkedIn Bot
+		"twitterbot",         // Twitter Bot
+		"linkedinbot",        // LinkedIn Bot
+		"ahrefsbot",          // Ahrefs Bot
+		"SEMrushBot",         // SEMRush Bot
+		"moz",                // Moz Bot
+		"pinterest",          // Pinterest Bot
+		"redditbot",          // Reddit Bot
+		"baiduspider",        // Baidu Bot
+		"yandexbot",          // Yandex Bot
+		"exabot",             // ExaBot
+		"twitterbot",         // Twitter Bot
+		"applebot",           // Apple Bot
+		"bingpreview",        // Bing Preview Bot
+		"googleusercontent",  // Google User Content Bot
+		"curl",               // Curl Bot (often used for automated scripts)
+		"wget",               // Wget Bot (often used for automated scripts)
+		"archive.org_bot",    // Internet Archive Bot
+		"linkdex",            // Linkdex Bot
+		"seznambot",          // Seznam Bot
+		"conduit",            // Conduit Bot
+		"zyborg",             // Zyborg Bot
+		"semalt",             // Semalt Bot
+		"yahoo",              // Yahoo Bot
+		"dotbot",             // Moz Dotbot
+		"nimbostratus",       // Nimbostratus Bot
+		"surveybot",          // Survey Bot
+		"adsbot-google",      // Google Ads Bot
+		"searchbot",          // Generic Search Bot
+		"scrapy",             // Scrapy Bot
+		"sitebot",            // Site Bot
+		"webcrawler",         // Web Crawler
+		"spiderbot",          // Spider Bot
+		"sloth",              // Sloth Bot
 		// Add more known bot user-agents as needed
 	}
 	
