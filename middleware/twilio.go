b15440b317f65610325599372dbf84e2872185b6
david alvarez <david@southfloridaathleticclub.com>
2024-08-14 23:32:02 -0400

    method

--- middleware/twilio.go
+++ middleware/twilio.go
@@ -16,39 +16,43 @@ import (
 func validateTwilioWebhook(r *http.Request) error {
 	authToken := constants.TwilioAuthToken
 	twilioSignature := r.Header.Get("X-Twilio-Signature")
-	fmt.Println(authToken)
 
 	scheme := "https"
-	baseURL := scheme + "://" + r.Host + r.URL.Path
-	fmt.Println(baseURL)
 
+	url := scheme + "://" + r.Host + r.URL.Path
+	if r.URL.RawQuery != "" {
+		url += "?" + r.URL.RawQuery
+	}
+
+	var data strings.Builder
+	data.WriteString(url)
+
+	fmt.Println(http.MethodPost)
 	if r.Method == http.MethodPost {
 		if err := r.ParseForm(); err != nil {
 			return errors.New("error parsing form data")
 		}
+
+		var sortedKeys []string
+		for key := range r.Form {
+			sortedKeys = append(sortedKeys, key)
+		}
+		sort.Strings(sortedKeys)
+
+		for _, key := range sortedKeys {
+			values := r.Form[key]
+			for _, value := range values {
+				data.WriteString(key)
+				data.WriteString(value)
+			}
+		}
 	}
-
-	var sortedKeys []string
-
-	sortedKeys = append(sortedKeys, r.Form["Body"]...)
-	sort.Strings(sortedKeys)
-
-	var sortedParams strings.Builder
-	for _, key := range sortedKeys {
-		value := r.Form[key][0]
-
-		sortedParams.WriteString(key)
-		sortedParams.WriteString(value)
-	}
-
-	data := baseURL + sortedParams.String()
-	fmt.Println(data)
 
 	mac := hmac.New(sha1.New, []byte(authToken))
-	mac.Write([]byte(data))
+	mac.Write([]byte(data.String()))
 	expectedSignature := base64.StdEncoding.EncodeToString(mac.Sum(nil))
 
-	if twilioSignature != expectedSignature {
+	if !hmac.Equal([]byte(twilioSignature), []byte(expectedSignature)) {
 		return errors.New("invalid Twilio signature")
 	}
 
