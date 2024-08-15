8fbfce4120e3a4ac05517c6317cdf4d3cd76d3d6
david alvarez <david@southfloridaathleticclub.com>
2024-08-14 22:55:44 -0400

    twilio prod

--- middleware/twilio.go
+++ middleware/twilio.go
@@ -6,6 +6,7 @@ import (
 	"encoding/base64"
 	"errors"
 	"net/http"
+	"sort"
 	"strings"
 
 	"github.com/davidalvarez305/yd_vending/constants"
@@ -15,16 +16,28 @@ func validateTwilioWebhook(r *http.Request) error {
 	authToken := constants.TwilioAuthToken
 	twilioSignature := r.Header.Get("X-Twilio-Signature")
 
-	url := "https://" + r.Host + r.URL.Path
-	if r.URL.RawQuery != "" {
-		url += "?" + r.URL.RawQuery
+	scheme := "https"
+	if r.TLS == nil {
+		scheme = "http"
 	}
 
-	data := url
-	if r.Method == "POST" {
-		r.ParseForm()
-		data += strings.Join(r.Form["Body"], "")
+	baseURL := scheme + "://" + r.Host + r.URL.Path
+
+	if r.Method == http.MethodPost {
+		if err := r.ParseForm(); err != nil {
+			return errors.New("error parsing form data")
+		}
 	}
+
+	var sortedParams []string
+	for key, values := range r.Form {
+		for _, value := range values {
+			sortedParams = append(sortedParams, key+value)
+		}
+	}
+	sort.Strings(sortedParams)
+
+	data := baseURL + strings.Join(sortedParams, "")
 
 	mac := hmac.New(sha1.New, []byte(authToken))
 	mac.Write([]byte(data))
