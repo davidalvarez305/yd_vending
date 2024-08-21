a5f4b3a330a1b3fde01f6cad8d386535fc73b830
David Alvarez <david.a@casautomotive.com>
2024-08-21 15:47:09 -0400

    updated csp

--- middleware/security.go
+++ middleware/security.go
@@ -42,7 +42,7 @@ func SecurityMiddleware(next http.Handler) http.Handler {
 		script-src-elem 'self' https://jspm.dev https://www.googletagmanager.com 'nonce-%s' https://connect.facebook.net;
 		style-src 'self' %s;
 		img-src 'self' https://www.google-analytics.com data: https://cdn.tailkit.com https://www.facebook.com https://www.google.com %s;
-		connect-src 'self' https://www.google-analytics.com https://www.googleadservices.com;
+		connect-src 'self' https://www.google-analytics.com https://www.googleadservices.com https://www.google.com;
 		style-src-elem 'self' https://fonts.bunny.net %s;
 		style-src-attr 'self' 'unsafe-inline';`, constants.AWSStorageBucket, nonceBase64, nonceBase64, constants.AWSStorageBucket, constants.AWSStorageBucket, constants.AWSStorageBucket)
 
