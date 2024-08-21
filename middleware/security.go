504cb2b5c3b9060c5c3773ddc2390e9041432375
david alvarez <david@southfloridaathleticclub.com>
2024-08-21 19:05:41 -0400

    csp

--- middleware/security.go
+++ middleware/security.go
@@ -38,11 +38,11 @@ func SecurityMiddleware(next http.Handler) http.Handler {
 		cspDirective := fmt.Sprintf(`default-src 'self';
 		script-src 'self' https://www.googletagmanager.com %s 'nonce-%s';
 		font-src 'self' https://fonts.bunny.net;
-		frame-src 'self' https://td.doubleclick.net/;
+		frame-src 'self' https://td.doubleclick.net https://www.facebook.com;
 		script-src-elem 'self' https://jspm.dev https://www.googletagmanager.com 'nonce-%s' https://connect.facebook.net;
 		style-src 'self' %s;
 		img-src 'self' https://www.google-analytics.com data: https://cdn.tailkit.com https://www.facebook.com https://www.google.com https://adservice.google.com https://www.googletagmanager.com %s;
-		connect-src 'self' https://www.google-analytics.com https://www.googleadservices.com https://www.google.com https://adservice.google.com;
+		connect-src 'self' https://www.google-analytics.com https://www.googleadservices.com https://www.google.com https://adservice.google.com https://www.facebook.com;
 		style-src-elem 'self' https://fonts.bunny.net %s;
 		style-src-attr 'self' 'unsafe-inline';`, constants.AWSStorageBucket, nonceBase64, nonceBase64, constants.AWSStorageBucket, constants.AWSStorageBucket, constants.AWSStorageBucket)
 
