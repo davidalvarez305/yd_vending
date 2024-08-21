e831f10d7e729153639786ad9daa6d931a4e2b75
David Alvarez <david.a@casautomotive.com>
2024-08-21 16:04:23 -0400

    comment out google phone call reporting

--- handlers/sms.go
+++ handlers/sms.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"github.com/davidalvarez305/yd_vending/constants"
-	"github.com/davidalvarez305/yd_vending/conversions"
 	"github.com/davidalvarez305/yd_vending/database"
 	"github.com/davidalvarez305/yd_vending/helpers"
 	"github.com/davidalvarez305/yd_vending/models"
@@ -148,7 +147,7 @@ func handleInboundCallEnd(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if phoneCall.CallDuration > 60 && dialStatus.DialCallStatus == "completed" {
+	/* if phoneCall.CallDuration > 60 && dialStatus.DialCallStatus == "completed" {
 		payload := types.GooglePayload{
 			ClientID: "",
 			UserId:   "",
@@ -160,7 +159,7 @@ func handleInboundCallEnd(w http.ResponseWriter, r *http.Request) {
 		}
 
 		conversions.SendGoogleConversion(payload)
-	}
+	} */
 
 	w.Header().Set("Content-Type", "application/xml")
 	w.WriteHeader(http.StatusOK)
