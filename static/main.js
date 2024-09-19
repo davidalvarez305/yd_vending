e0f0d915efa6f020412e80e18abccbb89474fb0a
David Alvarez <david.a@casautomotive.com>
2024-09-19 10:40:39 -0400

    preserve query string

--- static/main.js
+++ static/main.js
@@ -1,24 +1,41 @@
 function handleCloseAlertModal() {
-  const closeModalButton = document.querySelectorAll(".closeModal");
-  const newModal = document.getElementById("alertModal");
+	const closeModalButton = document.querySelectorAll(".closeModal");
+	const newModal = document.getElementById("alertModal");
 
-  closeModalButton.forEach((button) => {
-    let children = button.children;
-    Array.from(children).forEach((child) => {
-      child.setAttribute("name", button.name);
-    });
+	closeModalButton.forEach((button) => {
+		let children = button.children;
+		Array.from(children).forEach((child) => {
+			child.setAttribute("name", button.name);
+		});
 
-    button.addEventListener("click", () => (newModal.style.display = "none"));
-  });
+		button.addEventListener("click", () => (newModal.style.display = "none"));
+	});
 }
 
 function setUser() {
-  const user = {
-    landingPage: window.location.href,
-    referrer: document.referrer,
-  };
+	const user = {
+		landingPage: window.location.href,
+		referrer: document.referrer,
+	};
 
-  localStorage.setItem("user", JSON.stringify(user));
+	localStorage.setItem("user", JSON.stringify(user));
 }
 
 document.addEventListener("DOMContentLoaded", () => localStorage.getItem("user") ?? setUser());
+
+function preserveQuerystring() {
+	const links = document.querySelectorAll('a');
+
+	links.forEach(link => {
+		link.addEventListener('click', function () {
+			const qs = window.location.search;
+			if (qs.length > 0) {
+				const url = new URL(this.href, window.location.origin);
+				url.search = url.search ? `${url.search}&${qs.substring(1)}` : qs;
+				this.href = url.toString();
+			}
+		});
+	});
+}
+
+document.addEventListener("DOMContentLoaded", () => preserveQuerystring());
