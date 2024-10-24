4b5d850e17089b06b86a55ea586dc44d7d92fc8a
David Alvarez <david.a@casautomotive.com>
2024-10-24 12:35:10 -0400

    error modal logic

--- handlers/partials.go
+++ handlers/partials.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/davidalvarez305/yd_vending/constants"
+	"github.com/davidalvarez305/yd_vending/helpers"
+	"github.com/davidalvarez305/yd_vending/types"
 )
 
 func PartialsHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,6 +14,8 @@ func PartialsHandler(w http.ResponseWriter, r *http.Request) {
 		switch r.URL.Path {
 		case "/partials/pop-up-modal":
 			GetPopUpModal(w, r)
+		case "/partials/error-modal":
+			GetErrorModal(w, r)
 		default:
 			http.Error(w, "No partials found.", http.StatusNotFound)
 		}
@@ -26,4 +30,20 @@ func GetPopUpModal(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	http.ServeFile(w, r, constants.PARTIAL_TEMPLATES_DIR+fileName)
+}
+
+func GetErrorModal(w http.ResponseWriter, r *http.Request) {
+	errMessage := r.URL.Query().Get("err")
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	tmplCtx := types.DynamicPartialTemplate{
+		TemplateName: "error",
+		TemplatePath: constants.PARTIAL_TEMPLATES_DIR + "error_banner.html",
+		Data: map[string]any{
+			"Message": errMessage,
+		},
+	}
+
+	helpers.ServeDynamicPartialTemplate(w, tmplCtx)
 }
