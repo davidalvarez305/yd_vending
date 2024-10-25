9d044f0eeb7300c0bedc2cbec3a35e6a36052464
David Alvarez <david.a@casautomotive.com>
2024-10-25 17:30:57 -0400

    BRING BACK POPUP BECAUSE IT DOESNT AFFECT CONVERSIONS...

--- static/marketing.js
+++ static/marketing.js
@@ -4,279 +4,279 @@ let latitude = null;
 let longitude = null;
 
 function getHost(urlString) {
-  let url;
-  try {
-    url = new URL(urlString);
-  } catch (error) {
-    return "";
-  }
+	let url;
+	try {
+		url = new URL(urlString);
+	} catch (error) {
+		return "";
+	}
 
-  let host = url.hostname.toLowerCase();
+	let host = url.hostname.toLowerCase();
 
-  if (host.startsWith("www.")) {
-    host = host.slice(4);
-  }
+	if (host.startsWith("www.")) {
+		host = host.slice(4);
+	}
 
-  const parts = host.split(".");
+	const parts = host.split(".");
 
-  // Handle cases like ftp.google.com or ads.google.com
-  if (
-    parts.length > 2 &&
-    !["com", "net", "org", "edu", "gov", "mil", "int"].includes(
-      parts[parts.length - 2]
-    )
-  ) {
-    host = parts.slice(-3).join(".");
-  } else if (parts.length > 1) {
-    // Check if the last part is a two-letter country code
-    const lastPart = parts[parts.length - 1];
-    if (lastPart.length === 2 && lastPart !== "co") {
-      // 'co' is a special case like .co.uk, .co.in, etc.
-      host = parts.slice(-3).join(".");
-    } else {
-      host = parts.slice(-2).join(".");
-    }
-  }
+	// Handle cases like ftp.google.com or ads.google.com
+	if (
+		parts.length > 2 &&
+		!["com", "net", "org", "edu", "gov", "mil", "int"].includes(
+			parts[parts.length - 2]
+		)
+	) {
+		host = parts.slice(-3).join(".");
+	} else if (parts.length > 1) {
+		// Check if the last part is a two-letter country code
+		const lastPart = parts[parts.length - 1];
+		if (lastPart.length === 2 && lastPart !== "co") {
+			// 'co' is a special case like .co.uk, .co.in, etc.
+			host = parts.slice(-3).join(".");
+		} else {
+			host = parts.slice(-2).join(".");
+		}
+	}
 
-  return host;
+	return host;
 }
 
 function getClickId(qs) {
-  for (const key of clickIdKeys) {
-    if (qs.has(key)) return qs.get(key);
-  }
+	for (const key of clickIdKeys) {
+		if (qs.has(key)) return qs.get(key);
+	}
 }
 
 function isPaid(qs) {
-  for (const key of clickIdKeys) {
-    if (qs.has(key)) {
-      return true;
-    }
-  }
+	for (const key of clickIdKeys) {
+		if (qs.has(key)) {
+			return true;
+		}
+	}
 
-  return false;
+	return false;
 }
 
 function getMedium(referrer, qs) {
-  // No referrer means the user accessed the website directly
-  if (referrer.length === 0) return "direct";
+	// No referrer means the user accessed the website directly
+	if (referrer.length === 0) return "direct";
 
-  // Non-empty referrer and no querystring === organic
-  if (qs.size === 0) return "organic";
+	// Non-empty referrer and no querystring === organic
+	if (qs.size === 0) return "organic";
 
-  // Paid ads
-  if (isPaid(qs)) return "paid";
+	// Paid ads
+	if (isPaid(qs)) return "paid";
 
-  // Querystring + non-empty referrer and no click id === referral
-  return "referral";
+	// Querystring + non-empty referrer and no click id === referral
+	return "referral";
 }
 
 function getUserLocation() {
-  const options = {
-    enableHighAccuracy: true,
-    timeout: 3000,
-    maximumAge: 0,
-  };
+	const options = {
+		enableHighAccuracy: true,
+		timeout: 3000,
+		maximumAge: 0,
+	};
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      function (position) {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-      },
-      function (error) {
-        console.error("Error getting user location:", error.message);
-      },
-      options
-    );
-  } else {
-    console.error("Geolocation is not supported by your browser.");
-  }
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(
+			function (position) {
+				latitude = position.coords.latitude;
+				longitude = position.coords.longitude;
+			},
+			function (error) {
+				console.error("Error getting user location:", error.message);
+			},
+			options
+		);
+	} else {
+		console.error("Geolocation is not supported by your browser.");
+	}
 }
 
 function getChannel(referrerUrl) {
-  const displayNetworks = ["googleads.g.doubleclick.net"];
+	const displayNetworks = ["googleads.g.doubleclick.net"];
 
-  const searchEngines = [
-    { domain: "google" },
-    { domain: "bing" },
-    { domain: "yahoo" },
-    { domain: "ecosia" },
-    { domain: "duckduckgo" },
-    { domain: "yandex" },
-    { domain: "baidu" },
-    { domain: "naver" },
-    { domain: "ask.com" },
-    { domain: "adsensecustomsearchads" },
-    { domain: "aol" },
-    { domain: "brave" },
-  ];
+	const searchEngines = [
+		{ domain: "google" },
+		{ domain: "bing" },
+		{ domain: "yahoo" },
+		{ domain: "ecosia" },
+		{ domain: "duckduckgo" },
+		{ domain: "yandex" },
+		{ domain: "baidu" },
+		{ domain: "naver" },
+		{ domain: "ask.com" },
+		{ domain: "adsensecustomsearchads" },
+		{ domain: "aol" },
+		{ domain: "brave" },
+	];
 
-  const majorSocialNetworks = [
-    "facebook",
-    "instagram",
-    "twitter",
-    "linkedin",
-    "pinterest",
-    "snapchat",
-    "reddit",
-    "whatsapp",
-    "wechat",
-    "telegram",
-    "discord",
-    "vkontakte",
-    "weibo",
-    "line",
-    "kakaotalk",
-    "qq",
-    "viber",
-    "telegram",
-    "tumblr",
-    "flickr",
-    "meetup",
-    "tagged",
-    "badoo",
-    "myspace",
-  ];
+	const majorSocialNetworks = [
+		"facebook",
+		"instagram",
+		"twitter",
+		"linkedin",
+		"pinterest",
+		"snapchat",
+		"reddit",
+		"whatsapp",
+		"wechat",
+		"telegram",
+		"discord",
+		"vkontakte",
+		"weibo",
+		"line",
+		"kakaotalk",
+		"qq",
+		"viber",
+		"telegram",
+		"tumblr",
+		"flickr",
+		"meetup",
+		"tagged",
+		"badoo",
+		"myspace",
+	];
 
-  const majorVideoPlatforms = [
-    "youtube",
-    "tiktok",
-    "vimeo",
-    "dailymotion",
-    "twitch",
-    "bilibili",
-    "youku",
-    "rutube",
-    "vine",
-    "peertube",
-    "ig tv",
-    "veoh",
-    "metacafe",
-    "vudu",
-    "vidyard",
-    "rumble",
-    "bit chute",
-    "brightcove",
-    "viddler",
-    "vzaar",
-  ];
+	const majorVideoPlatforms = [
+		"youtube",
+		"tiktok",
+		"vimeo",
+		"dailymotion",
+		"twitch",
+		"bilibili",
+		"youku",
+		"rutube",
+		"vine",
+		"peertube",
+		"ig tv",
+		"veoh",
+		"metacafe",
+		"vudu",
+		"vidyard",
+		"rumble",
+		"bit chute",
+		"brightcove",
+		"viddler",
+		"vzaar",
+	];
 
-  // Check display platforms
-  for (let platform of displayNetworks) {
-    if (referrerUrl.includes(platform)) {
-      return "display";
-    }
-  }
+	// Check display platforms
+	for (let platform of displayNetworks) {
+		if (referrerUrl.includes(platform)) {
+			return "display";
+		}
+	}
 
-  // Check search engines
-  for (let engine of searchEngines) {
-    if (referrerUrl.includes(engine.domain)) {
-      return "search";
-    }
-  }
+	// Check search engines
+	for (let engine of searchEngines) {
+		if (referrerUrl.includes(engine.domain)) {
+			return "search";
+		}
+	}
 
-  // Check social networks
-  for (let network of majorSocialNetworks) {
-    if (referrerUrl.includes(network)) {
-      return "social";
-    }
-  }
+	// Check social networks
+	for (let network of majorSocialNetworks) {
+		if (referrerUrl.includes(network)) {
+			return "social";
+		}
+	}
 
-  // Check video platforms
-  for (let platform of majorVideoPlatforms) {
-    if (referrerUrl.includes(platform)) {
-      return "video";
-    }
-  }
+	// Check video platforms
+	for (let platform of majorVideoPlatforms) {
+		if (referrerUrl.includes(platform)) {
+			return "video";
+		}
+	}
 
-  return "other";
+	return "other";
 }
 
 function applyButtonlogic() {
-  let quoteButtons = document.querySelectorAll(".quoteButton");
+	let quoteButtons = document.querySelectorAll(".quoteButton");
 
-  quoteButtons.forEach((button) => {
-    let children = button.children;
-    Array.from(children).forEach((child) => {
-      child.setAttribute("name", button.name);
-    });
+	quoteButtons.forEach((button) => {
+		let children = button.children;
+		Array.from(children).forEach((child) => {
+			child.setAttribute("name", button.name);
+		});
 
-    button.addEventListener("click", function () {
-      form.scrollIntoView({ behavior: "smooth" });
-      form.querySelector("input, textarea, select").focus();
+		button.addEventListener("click", function () {
+			form.scrollIntoView({ behavior: "smooth" });
+			form.querySelector("input, textarea, select").focus();
 
-      const buttonClicked = document.getElementById("button_clicked");
-      buttonClicked.value = button.getAttribute("name");
+			const buttonClicked = document.getElementById("button_clicked");
+			buttonClicked.value = button.getAttribute("name");
 
-      const modal = document.getElementById("modalOverlay");
-      if (modal) modal.style.display = "none";
-    });
-  });
+			const modal = document.getElementById("modalOverlay");
+			if (modal) modal.style.display = "none";
+		});
+	});
 }
 
 function handleQuoteFormSubmit(e) {
-  e.preventDefault();
+	e.preventDefault();
 
-  // Retrieve user data and URL details
-  const user = JSON.parse(localStorage.getItem("user")) || {};
-  const url = new URL(user.landingPage || window.location.href);
-  const language = navigator.language || navigator.userLanguage;
-  const marketingParams = Object.fromEntries(url.searchParams);
+	// Retrieve user data and URL details
+	const user = JSON.parse(localStorage.getItem("user")) || {};
+	const url = new URL(user.landingPage || window.location.href);
+	const language = navigator.language || navigator.userLanguage;
+	const marketingParams = Object.fromEntries(url.searchParams);
 
-  const data = new FormData();
-  if (isPaid(url.searchParams))
-    data.append("click_id", getClickId(url.searchParams));
-  data.append("landing_page", user.landingPage);
-  data.append("referrer", user.referrer);
-  data.append("language", language);
+	const data = new FormData();
+	if (isPaid(url.searchParams))
+		data.append("click_id", getClickId(url.searchParams));
+	data.append("landing_page", user.landingPage);
+	data.append("referrer", user.referrer);
+	data.append("language", language);
 
-  // Append source, medium, and channel based on URL or referrer
-  const source = url.searchParams.get("source") || getHost(user.referrer);
-  const medium =
-    url.searchParams.get("medium") ||
-    getMedium(user.referrer, url.searchParams);
-  const channel = url.searchParams.get("channel") || getChannel(user.referrer);
-  if (source) data.append("source", source);
-  if (medium) data.append("medium", medium);
-  if (channel) data.append("channel", channel);
+	// Append source, medium, and channel based on URL or referrer
+	const source = url.searchParams.get("source") || getHost(user.referrer);
+	const medium =
+		url.searchParams.get("medium") ||
+		getMedium(user.referrer, url.searchParams);
+	const channel = url.searchParams.get("channel") || getChannel(user.referrer);
+	if (source) data.append("source", source);
+	if (medium) data.append("medium", medium);
+	if (channel) data.append("channel", channel);
 
-  // Handle geolocation (conditionally append if available)
-  if (longitude) data.append("longitude", longitude);
-  if (latitude) data.append("latitude", latitude);
+	// Handle geolocation (conditionally append if available)
+	if (longitude) data.append("longitude", longitude);
+	if (latitude) data.append("latitude", latitude);
 
-  // Append all marketing parameters and form values
-  Object.entries(marketingParams).forEach(
-    ([key, value]) => value && data.append(key, value)
-  );
-  new FormData(form).forEach((value, key) => value && data.append(key, value));
+	// Append all marketing parameters and form values
+	Object.entries(marketingParams).forEach(
+		([key, value]) => value && data.append(key, value)
+	);
+	new FormData(form).forEach((value, key) => value && data.append(key, value));
 
-  const alertModal = document.getElementById("alertModal");
-  fetch("/quote", {
-    method: "POST",
-    credentials: "include",
-    body: data,
-  })
-    .then(response => {
-      const token = response.headers.get("X-Csrf-Token");
-      if (token) {
-        const tokens = document.querySelectorAll('[name="csrf_token"]');
-        tokens.forEach((csrf_token) => (csrf_token.value = token));
-      }
-      if (response.ok) {
-        return response.text();
-      } else {
-        return response.text().then((err) => {
-          throw new Error(err);
-        });
-      }
-    })
-    .then((html) => (alertModal.outerHTML = html))
-    .catch((err) => (alertModal.outerHTML = err))
-    .finally(() => {
-      handleCloseAlertModal();
-      form.reset();
-    });
+	const alertModal = document.getElementById("alertModal");
+	fetch("/quote", {
+		method: "POST",
+		credentials: "include",
+		body: data,
+	})
+		.then(response => {
+			const token = response.headers.get("X-Csrf-Token");
+			if (token) {
+				const tokens = document.querySelectorAll('[name="csrf_token"]');
+				tokens.forEach((csrf_token) => (csrf_token.value = token));
+			}
+			if (response.ok) {
+				return response.text();
+			} else {
+				return response.text().then((err) => {
+					throw new Error(err);
+				});
+			}
+		})
+		.then((html) => (alertModal.outerHTML = html))
+		.catch((err) => (alertModal.outerHTML = err))
+		.finally(() => {
+			handleCloseAlertModal();
+			form.reset();
+		});
 }
 
 document.addEventListener("DOMContentLoaded", getUserLocation());
--- static/popup.js
+++ static/popup.js
@@ -5,60 +5,60 @@ let hasScrolledUpAbove50 = false;
 let alreadyPoppedUp = false;
 
 function showModal() {
-  fetch("/partials/pop-up-modal")
-    .then((response) => {
-      if (response.ok) {
-        return response.text();
-      } else {
-        throw new Error("Error: " + response.statusText);
-      }
-    })
-    .then((modalHTML) => {
-      const modalOverlay = document.getElementById("modalOverlay");
-      modalOverlay.outerHTML = modalHTML;
-      modalOverlay.style.display = "flex";
+	fetch("/partials/pop-up-modal")
+		.then((response) => {
+			if (response.ok) {
+				return response.text();
+			} else {
+				throw new Error("Error: " + response.statusText);
+			}
+		})
+		.then((modalHTML) => {
+			const modalOverlay = document.getElementById("modalOverlay");
+			modalOverlay.outerHTML = modalHTML;
+			modalOverlay.style.display = "flex";
 
-      const modalContent = document.getElementById("modalContent");
+			const modalContent = document.getElementById("modalContent");
 
-      // Prevent clicks inside the modal content from closing the modal
-      modalContent.addEventListener("click", (e) => {
-        e.stopPropagation();
-      });
+			// Prevent clicks inside the modal content from closing the modal
+			modalContent.addEventListener("click", (e) => {
+				e.stopPropagation();
+			});
 
-      // Hide the modal when clicking outside the modal content
-      const modal = document.getElementById("modalOverlay");
-      modal.addEventListener("click", () => {
-        modal.outerHTML = "";
-        alreadyPoppedUp = true;
-      });
+			// Hide the modal when clicking outside the modal content
+			const modal = document.getElementById("modalOverlay");
+			modal.addEventListener("click", () => {
+				modal.outerHTML = "";
+				alreadyPoppedUp = true;
+			});
 
-      // Bring form to focus
-      applyButtonlogic();
-    })
-    .catch(console.error);
+			// Bring form to focus
+			applyButtonlogic();
+		})
+		.catch(console.error);
 }
 
 // Trigger after time
 window.addEventListener("load", () => {
-  setTimeout(() => {
-    if (!alreadyPoppedUp) showModal();
-  }, TRIGGER_THRESHOLD);
+	setTimeout(() => {
+		if (!alreadyPoppedUp) showModal();
+	}, TRIGGER_THRESHOLD);
 });
 
 // Trigger after scroll
 window.addEventListener("scroll", function () {
-  const pageHeight = document.documentElement.scrollHeight;
-  const scrollPercent = window.scrollY / (pageHeight - window.innerHeight);
+	const pageHeight = document.documentElement.scrollHeight;
+	const scrollPercent = window.scrollY / (pageHeight - window.innerHeight);
 
-  if (scrollPercent >= 0.75) {
-      hasScrolledPast75 = true;
-  }
+	if (scrollPercent >= 0.75) {
+		hasScrolledPast75 = true;
+	}
 
-  // Check if scrolled back above 50% and conditions are met
-  if (hasScrolledPast75 && scrollPercent < 0.50) {
-      if (!hasScrolledUpAbove50 && !alreadyPoppedUp) {
-          showModal();
-          hasScrolledUpAbove50 = true; // Ensure the alert only fires once
-      }
-  }
+	// Check if scrolled back above 50% and conditions are met
+	if (hasScrolledPast75 && scrollPercent < 0.50) {
+		if (!hasScrolledUpAbove50 && !alreadyPoppedUp) {
+			showModal();
+			hasScrolledUpAbove50 = true; // Ensure the alert only fires once
+		}
+	}
 });
