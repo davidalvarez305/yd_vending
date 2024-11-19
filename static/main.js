221fed74277740456f17de77d82e621d10474cd1
David Alvarez <david.a@casautomotive.com>
2024-11-19 13:57:04 -0500

    null

--- static/main.js
+++ static/main.js
@@ -11,8 +11,8 @@ function handleCloseAlertModal() {
 
 function setUser() {
     const user = {
-        landingPage: typeof window !== "undefined" && window.location ? window.location.href : null,
-        referrer: typeof document !== "undefined" && document.referrer ? document.referrer : null,
+        landingPage: typeof window !== "undefined" && window.location ? window.location.href : "",
+        referrer: typeof document !== "undefined" && document.referrer ? document.referrer : "",
     };
 
     localStorage.setItem("user", JSON.stringify(user));
