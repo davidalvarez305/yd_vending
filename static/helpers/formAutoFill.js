43bb926e75ba133545b2a0bc8f36143bf6058707
David Alvarez <david.a@casautomotive.com>
2024-11-25 15:09:29 -0500

    redirect to mini site

---
+++ static/helpers/formAutoFill.js
@@ -0,0 +1,11 @@
+export function formAutofill() {
+    const params = new URLSearchParams(window.location.search);
+
+    for (const [key, value] of Object.entries(params)) {
+        const field = document.querySelector(key);
+
+        if (field && field.closest('form')) {
+            field.value = value;
+        }
+    }
+}
