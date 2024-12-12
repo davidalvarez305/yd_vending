8cc4c38d5b74d0bed0341311a3f117b2c7dbf42a
David Alvarez <david.a@casautomotive.com>
2024-12-12 11:26:50 -0500

    global variables

--- static/helpers/phoneConversions.js
+++ static/helpers/phoneConversions.js
@@ -1,16 +1,20 @@
+const facebookLeadEventName = document.getElementById("facebookLeadEventName").textContent;
+const linkedInEventID = document.getElementById("linkedInEventID").textContent;
+const quoteEventName = document.getElementById("quoteEventName").textContent;
+
 const phoneNumber = document.getElementById("phoneNumberCTA");
 
-phoneNumber.addEventListener("click", () => handlePhoneNumberClick())
+if (phoneNumber) phoneNumber.addEventListener("click", () => handlePhoneNumberClick())
 
 function handlePhoneNumberClick() {
     // Report FB Conversion
-    if (fbq) fbq("track", "{{ .LeadEventName }}");
+    if (fbq) fbq("track", facebookLeadEventName);
 
     // Report LinkedIn
-    if (window.lintrk) window.lintrk('track', { conversion_id: "{{ .LinkedInEventID }}" });
+    if (window.lintrk) window.lintrk('track', { conversion_id: linkedInEventID });
 
     // Report Microsoft Ads Conversion
     window.uetq = window.uetq || [];
 
-    window.uetq.push('event', '{{ .QuoteEventName }}', {});
+    window.uetq.push('event', quoteEventName, {});
 }
