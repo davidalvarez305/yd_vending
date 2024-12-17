90947ea1b28582f00a8f976c9f7fbec449be62da
unknown <david@southfloridaathleticclub.com>
2024-12-16 23:07:49 -0500

    location contact

--- types/types.go
+++ types/types.go
@@ -414,7 +414,6 @@ type LocationForm struct {
 	VendingLocationID    *int     `json:"vending_location_id" form:"vending_location_id" schema:"vending_location_id"`
 	LocationStatusID     *int     `json:"location_status_id" form:"location_status_id" schema:"location_status_id"`
 	BusinessID           *int     `json:"business_id" form:"business_id" schema:"business_id"`
-	LocationContact      *int     `json:"location_contact_id" form:"location_contact_id" schema:"location_contact_id"`
 	DateStarted          int64    `json:"date_started" form:"date_started" schema:"date_started"`
 	Name                 *string  `json:"name" form:"name" schema:"name"`
 	Longitude            *float64 `json:"longitude" form:"longitude" schema:"longitude"`
